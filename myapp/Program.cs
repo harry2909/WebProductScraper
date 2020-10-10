8ec5e7d2b53cf515f93c48dc9d5db841d354862c
Harry <hpreston2909@gmail.com>
2020-10-10 22:41:41 +0100

    Output changed

--- myapp/Program.cs
+++ myapp/Program.cs
@@ -6,7 +6,8 @@ namespace myapp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello cruel World!");
+            Console.WriteLine("New Line");
         }
     }
 }
