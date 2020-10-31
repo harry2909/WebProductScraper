7e26293044d3f770c637a2e8b80eb970ae863775
Harry <hpreston2909@gmail.com>
2020-10-31 13:52:44 +0000

    Final commit for now. All seems to be working fine.

--- scraper.py
+++ scraper.py
@@ -1,6 +1,8 @@
+#!/usr/bin/env python
 import requests
 from bs4 import BeautifulSoup
 import smtplib
+import time
 
 URL = 'https://www.amazon.co.uk/PHANTEKS-Eclipse-P400A-Midi-Tower-Tempered/dp/B07TTDW37F/ref=sr_1_4?crid=2V5KCVVGQ9NGS&dchild=1&keywords=phanteks+p400a&qid=1604147817&sprefix=phanteks%2Caps%2C217&sr=8-4'
 
@@ -49,6 +51,7 @@ def send_mail():
 
     server.quit()
 
-
-check_price()
+while(True):
+    check_price()
+    time.sleep(60 * 60)
 
